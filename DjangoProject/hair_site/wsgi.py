1a6d2ab18ea3a0f7641ed3ce7f971d175f86cf48
maksim <Brodiaga1991@gmail.com>
2023-05-30 15:25:58 +0200

    black .

--- DjangoProject/hair_site/wsgi.py
+++ DjangoProject/hair_site/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hair_site.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hair_site.settings")
 
 application = get_wsgi_application()
